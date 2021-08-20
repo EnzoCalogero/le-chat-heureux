147c8e98defa848b67f1edd6fd5e25cd89c4f30a
Enzo Calogero <EnzoCalogero@users.noreply.github.com>
2021-08-20 22:51:44 +0200

    version 2

--- core/cat_laser_sans_pca_v2/cat_laser_sans_pca_v2.ino
+++ core/cat_laser_sans_pca_v2/cat_laser_sans_pca_v2.ino
@@ -9,10 +9,20 @@ Servo myservo1;  // create servo object to control a servo
 int D0 = 2; // Left eye
 int D1 = 3; // Right eye
 
-        int base_a=10;
-        int base_b=170;
-        int high_a=100;
-        int high_b=170;
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 4;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+int center_high=120;
+int base_a=10;
+int base_b=170;
+int high_a=100;
+int high_b=170;
+
+
 /*
  * angleToPulse(int ang)
  * gets angle in degree and returns the pulse width
@@ -21,31 +31,7 @@ int D1 = 3; // Right eye
  */
 
 
-// create a circular tragit with randmess 
-void base() {
 
-  int high=130;
-  int high_=0;
-  int speed_dist;
-  speed_dist=random(0,400);
-  for( int base =0; base<141; base +=10){
-    high_= high+random(-12, 12);
-  //  pwm.setPWM(1, 0, angleToPulse(base) );
-  myservo1.write(base);
-  
-  //  pwm.setPWM(0, 0, angleToPulse(high_) );
-    myservo0.write(high_);
-    delay(200 + random(0,400));
-  }
-  for( int base =181; base>1; base -=10){
-    high_= high+random(-12, 12);
-   // pwm.setPWM(1, 0, angleToPulse(base) );
-   // pwm.setPWM(0, 0, angleToPulse(high_) );
-    myservo1.write(base);
-    myservo0.write(high_);
-    delay(200 + random(0,400));
-  }
-}
 
 
 void traject(){
@@ -55,7 +41,7 @@ void traject(){
   int high;
   int base_a=random(30,140);
   int high_a=random(120,160);
-  while(true){
+  while(check_button()==1){
     int base_b=random(30,140);
     int high_b=random(120,160);
     for( int i =0; i<10; i +=1){
@@ -68,29 +54,32 @@ void traject(){
         myservo1.write(base);
         myservo0.write(high);
         delay(random(50,500)); //200
+        
         }
     base_a=base_b;
     high_a=high_b;
   }
 }
 
+
+
 void box_drow(void)
 {       
         // basso dx
         myservo1.write(base_a);
-        myservo0.write(high_a);
+        myservo0.write(center_high);
         delay(1000);
         // basso sx
         myservo1.write(base_b);
-        myservo0.write(high_a);
+        myservo0.write(center_high);
         delay(1000);
         // alto sx
         myservo1.write(base_b);
-        myservo0.write(high_b);
+        myservo0.write(center_high);
         delay(1000);
         // alto dx
         myservo1.write(base_a);
-        myservo0.write(high_b);
+        myservo0.write(center_high);
         delay(1000);
   }
 
@@ -106,12 +95,46 @@ void setup() {
   myservo1.attach(10);  // attaches the servo on pin 9 to the servo object
  // pwm.begin();
  // pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+
+int check_button(void) {
+buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    return 1;
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+    return 0;
+  }
 }
 
 
 void loop() {
 
-traject();
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (check_button()) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    traject();
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+    box_drow();
+  } 
+//temp change
+//center_high=center_high+5;
+
+//traject();
 //base();
 //box_drow();
 }
