50e46b758547b3f73fa9bec5f6b39a0aa2875350
Enzo Calogero <EnzoCalogero@users.noreply.github.com>
2021-09-06 23:28:18 +0200

    06 09 2021

--- core/cat_laser_sans_pca_v2/cat_laser_sans_pca_v2.ino
+++ core/cat_laser_sans_pca_v2/cat_laser_sans_pca_v2.ino
@@ -1,49 +1,38 @@
-//#include <Wire.h>
 #include <Servo.h>
-
+// our servo # counter
 Servo myservo0;  // create servo object to control a servo
 Servo myservo1;  // create servo object to control a servo
 
-// our servo # counter
-//uint8_t servonum = 0;
+
+
 int D0 = 2; // Left eye
 int D1 = 3; // Right eye
-
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin = 4;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
-int base_a=10;
-int base_b=170;
-int high_a=100;
-int high_b=170;
+//int base_a=10;
+//int base_b=170;
+//int high_a=100;
+//int high_b=170;
 
 // states variables
-int delta_x = 40;
-int high_center =90;
-int lateral_x=20;
-int base_l=90;
-/*
- * angleToPulse(int ang)
- * gets angle in degree and returns the pulse width
- * also prints the value on seial monitor
- * written by Ahmad Nejrabi for Robojax, Robojax.com
- */
-
-
-
-
+//int delta_x = 40;
+int high_center;
+int lateral_x;
+int base_l;
 
 void traject(){
+high_center = analag_read_h();
+lateral_x = analag_read_x();
   int delta_base;
   int delta_high;
   int base;
   int high;
-  int base_a=random(30,140);
-  int high_a=random(120,160);
+  int base_a=random(base_l - lateral_x, base_l + lateral_x);
+  int high_a=random(high_center-45, high_center );
   while(check_button()==1){
     int base_b=random(30,140);
     int high_b=random(120,160);
@@ -52,8 +41,6 @@ void traject(){
         delta_high=(high_a - high_b)/10;
         base=base_a-delta_base*i;
         high=high_a-delta_high*i;
-        //pwm.setPWM(1, 0, angleToPulse(base) );
-        //pwm.setPWM(0, 0, angleToPulse(high) );
         myservo1.write(base);
         myservo0.write(high);
         delay(random(50,500)); //200
@@ -64,29 +51,21 @@ void traject(){
   }
 }
 
-
-
-void box_drow(void)
+void box_drow1(void)
 {       
-high_center = analag_read_h();
-lateral_x = analag_read_x();
-
+        high_center = analag_read_h();
+        lateral_x = analag_read_x();
         // basso dx
         myservo1.write(base_l + lateral_x);
-        myservo0.write(high_center - delta_x);
+        myservo0.write(high_center);
         delay(500);
         // basso sx
+        high_center = analag_read_h();
+        lateral_x = analag_read_x();
         myservo1.write(base_l-lateral_x);
-        myservo0.write(high_center - delta_x);
+        myservo0.write(high_center);
         delay(500);
         // alto sx
-        myservo1.write(base_l-lateral_x);
-        myservo0.write(high_center + delta_x);
-        delay(500);
-        // alto dx
-        myservo1.write(base_l + lateral_x);
-        myservo0.write(high_center + delta_x);
-        delay(500);
   }
 
 
@@ -99,11 +78,6 @@ void setup() {
   digitalWrite(D1, HIGH); 
   myservo0.attach(9);  // attaches the servo on pin 9 to the servo object
   myservo1.attach(10);  // attaches the servo on pin 9 to the servo object
- // pwm.begin();
- // pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
 }
@@ -111,51 +85,38 @@ void setup() {
 
 int check_button(void) {
 buttonState = digitalRead(buttonPin);
-
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
     // turn LED on:
-    digitalWrite(ledPin, HIGH);
-    return 1;
+  digitalWrite(D0, HIGH);   // turn the LED on (HIGH is the voltage level)
+ // digitalWrite(D1, HIGH); ;
+  return 1;
   } else {
     // turn LED off:
-    digitalWrite(ledPin, LOW);
-    return 0;
+  digitalWrite(D0, LOW);   // turn the LED on (HIGH is the voltage level)
+ // digitalWrite(D1, HIGH); 
+  return 0;
   }
 }
 
 int analag_read_h() {
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float high_center = floor(sensorValue * (170.0 / 1023.0));
-  // print out the value you read:
-  Serial.println(high_center);
-  Serial.println(sensorValue);
+  high_center = map(sensorValue, 0, 1023, 60, 170);
   return high_center;
 }
 
 int analag_read_x() {
   // read the input on analog pin 1:
   int sensorValue = analogRead(A1);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float lenght_x = floor(sensorValue * (60.0 / 1023.0));
-  // print out the value you read:
-  Serial.println(lenght_x);
-  Serial.println(sensorValue);
+  int lenght_x = map(sensorValue, 0, 1023, 0, 70);
   return lenght_x;
 }
 
-
 void loop() {
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-//  if (check_button()) {
-//    // turn LED on:
-//    digitalWrite(ledPin, HIGH);
-//    traject();
-//  } else {
-//    // turn LED off:
-//    digitalWrite(ledPin, LOW);
-    box_drow();
- 
+if (check_button()) {
+    traject();
+  } else {
+    box_drow1();
+  }
 }
